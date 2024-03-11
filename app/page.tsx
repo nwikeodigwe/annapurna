c1649cbdf5f9ba77e6eaf120f43f5667fe83b2ed
Nwike Odigwe <nwikeodigwe@outlook.com>
2024-03-11 10:47:51 +0100

    Construction page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export default function Home() {
+  return (
+    <main
+      className={`flex ${styles.background} bg-cover bg-center min-h-screen`}
+    ></main>
+  );
+}
